81abfbff8ef64e99149fe4fded0a37a2d2af69bf
oscamengual <o.menguaal@gmail.com>
2020-11-02 19:45:15 +0100

    Readme

--- model/zona_camarero.php
+++ model/zona_camarero.php
@@ -1,1 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum=1.0"/>
+        <link  rel="stylesheet" href="zona_camarero.css"/>
+        <title> </title>
+    </head>
+    <body>
+        <h1>Mesas</h1>
+        <div class="container">
+            <div class="item">1</div>
+            <div class="item">2</div>
+            <div class="item">3</div>
+            <div class="item">4</div>
+            <div class="item">5</div>
+            <div class="item">6</div>
+            <div class="item">7</div>
+            <div class="item">8</div>
+            <div class="item">9</div>
+            <div class="item">10</div>
+            <div class="item">11</div>
+            <div class="item">12</div>
+        </div>
+    </body>
+</html>
