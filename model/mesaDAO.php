b8722b80f22ec7fab659d5ea02241ddae1fdda9d
Eliot1027 <71654992+Eliot1027@users.noreply.github.com>
2020-11-03 17:00:31 +0100

    funcion filtros prueba1

--- model/mesaDAO.php
+++ model/mesaDAO.php
@@ -24,4 +24,27 @@ class MesaDAO{
             echo "</div>";
         }
     }
+
+    public function filtrarMesas($num_comensales,$ubicavion,$disponibilidad){
+
+        $sql="SELECT DISTINCT * FROM tbl_mesa INNER JOIN tbl_ubicacion ON tbl_mesa.id_ubicacion = tbl_ubicacion.id_ubicacion WHERE tbl_mesa.capacidad_mesa=? AND tbl_mesa.Disponibilidad=? AND tbl_ubicacion.id_ubicacion=? ;";
+        $sentencia=$this->pdo->prepare($sql);
+        $sentencia->bindParam(1,$num_comensales);
+        $sentencia->bindParam(3,$disponibilidad);
+        $sentencia->bindParam(2,$ubicavion);
+        
+        $sentencia->execute();
+
+        $lista_mesas=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($lista_mesas as $mesa) {
+            echo "<div class='item'";
+            $id=$mesa['id_mesa']." ";
+            echo "<p>{$mesa['capacidad_mesa']}"." Comensales <br>";
+            echo "{$mesa['Disponibilidad']}<br>";
+            echo "{$mesa['Nombre_ubicacion']}</p>";
+            echo "</div>";
+        }
+
+    }
 }
--- model/zona_camarero.php
+++ model/zona_camarero.php
@@ -7,6 +7,34 @@
         <title> </title>
     </head>
     <body>
+
+        <form action="/action_page.php">
+
+            <label for="num_com">numero_comensales</label>
+            <select name="num_com">
+                <option value="2">2 asientos</option> 
+                <option value="4">4 asientos</option> 
+                <option value="6">2 asientos</option>
+                <option value="8">2 asientos</option> 
+            </select>
+
+            <label for="Disponibilidad">Disponibilidad</label>
+            <select name="Disponibilidad">
+                <option value="Disponible">Disponible</option> 
+                <option value="Reservada">Reservada</option> 
+            </select>
+
+            <label for="ubicacion">Ubicacion del Restaurante</label>
+            <select name="ubicacion">
+                <option value="1">Comedor-1</option> 
+                <option value="2">Comedor-2</option> 
+                <option value="3">Terraza</option>
+                <option value="4">Sala privada</option> 
+            </select>
+            <input type="submit" value="Submit">
+
+        </form>
+        
         <h1>Mesas</h1>
         <div class="container">
         <?php
