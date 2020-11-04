a5d9ed5a2cc881f4eb71e6f17e2aa7a621311918
Albert Buendia <6235.joan23@fje.edu>
2020-11-04 17:12:06 +0100

    Subida

--- model/mesaDAO.php
+++ model/mesaDAO.php
@@ -24,7 +24,7 @@ class MesaDAO{
             $id=$mesa['id_mesa']." ";
             echo "<p>{$mesa['capacidad_mesa']}"." Comensales <br>";
             echo "{$mesa['Disponibilidad']}<br>";
-            echo "<form action='wfjowiefngowsbnv.php?id_de_la_mesa={$id}' method='POST'>";
+            echo "<form action='mesaDAO.php?id_de_la_mesa={$id}' method='POST'>";
             echo "<select name='disponibilidad'>";
             echo "<option value='Disponible'>Disponible</option> ";
             echo "<option value='Reservada'>Reservada</option> ";
@@ -68,6 +68,12 @@ class MesaDAO{
         }
         echo "</div>";
 
+    }
+
+    public function update(){
+        $estado=$_POST['disponibilidad'];
+        $id_mesa=$_GET['id_de_la_mesa'];
+        $query="UPDATE `tbl_mesa` SET `Disponibilidad` = $estado WHERE `tbl_mesa`.`id_mesa` = $id_mesa;";
     }
 
 }
