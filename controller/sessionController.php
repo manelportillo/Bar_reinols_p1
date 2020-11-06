db7eb21f92405bd8a81a4a8d360ee21f21c60730
oscamengual <o.menguaal@gmail.com>
2020-11-06 19:31:16 +0100

    Último css

--- controller/sessionController.php
+++ controller/sessionController.php
@@ -5,8 +5,8 @@ if (!isset($_SESSION['camarero'])) {
     header('Location:../index.php');
 }
 // var_dump($_SESSION['camarero']);
-echo '<div class="logo">
-        <h2>Bienvenido '.$_SESSION['camarero']->getEmail().'</h2>
-        <h3 class="logout"style="float: right;"><a href="../controller/logoutController.php">Logout</a></h3>
-        </div>';
+echo '<div class="logo">';
+        echo '<h2>Bienvenido '.$_SESSION['camarero']->getEmail().'</h2>';
+        echo '<h3 class="logout"><a href="../controller/logoutController.php">Logout</a></h3>';
+        echo '</div>';
 ?>
